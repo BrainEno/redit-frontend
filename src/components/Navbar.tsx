19b0c756baf58b1c7b303c7c0a594f59e446c2db
Bruce Bryant <793041663@qq.com>
2021-01-28 23:22:14 +0800

    Search Subs

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Logo from "../../public/logo.svg";
 import Link from "next/link";
 import Axios from "axios";
+import { Sub } from "types";
 import { useAuthDispatch, useAuthState } from "../context/auth";
+import { useRouter } from "next/router";
 
 const Navbar: React.FC = () => {
+  const [name, setName] = useState("");
+  const [subs, setSubs] = useState<Sub[]>([]);
+  const [timer, setTimer] = useState(null);
   const { authenticated, loading } = useAuthState();
   const dispatch = useAuthDispatch();
+  const router = useRouter();
 
   const logout = () => {
     Axios.get("/auth/logout")
@@ -15,6 +21,33 @@ const Navbar: React.FC = () => {
         window.location.reload();
       })
       .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    if (name.trim() === "") {
+      setSubs([]);
+    }
+    searchSubs();
+  }, [name]);
+
+  const searchSubs = async () => {
+    clearTimeout(timer);
+    setTimer(
+      setTimeout(async () => {
+        try {
+          const { data } = await Axios.get(`/subs/search/${name}`);
+          setSubs(data);
+          console.log(data);
+        } catch (err) {
+          console.log(err);
+        }
+      }, 250)
+    );
+  };
+
+  const goToSub = (subName: string) => {
+    router.push(`/r/${subName}`);
+    setName("");
   };
 
   return (
@@ -31,13 +64,36 @@ const Navbar: React.FC = () => {
         </span>
       </div>
       {/*Search Input*/}
-      <div className='flex items-center mx-auto bg-gray-100 border rounded hover:bg-white hover:border-blue-500'>
+      <div className='relative flex items-center mx-auto bg-gray-100 border rounded hover:bg-white hover:border-blue-500'>
         <i className='pl-4 pr-3 text-gray-500 fas fa-search'></i>
         <input
           type='text'
           className='py-1 pr-3 bg-transparent rounded focus:outline-none w-160'
           placeholder='搜索...'
+          value={name}
+          onChange={(e) => setName(e.target.value)}
         />
+        <div
+          className='absolute left-0 right-0 bg-white '
+          style={{ top: "100%" }}>
+          {subs?.map((sub) => (
+            <div
+              className='flex items-center px-4 py-3 cursor-pointer hover:bg-gray-200'
+              key={sub.title}
+              onClick={() => goToSub(sub.name)}>
+              <img
+                src={sub.imageUrl}
+                alt='sub'
+                className='mr-4 rounded-full'
+                style={{ height: "32px", width: "32px" }}
+              />
+              <div className='ml-4 text-sm'>
+                <p className='font-medium'>{sub.name}</p>
+                <p className='text-gray-600'>{sub.title}</p>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
       {/*Auth buttons*/}
       <div className='flex'>
--- src/pages/r/[sub]/submit.tsx
+++ src/pages/r/[sub]/submit.tsx
@@ -1,4 +1,5 @@
 import Axios from "axios";
+import { GetServerSideProps } from "next";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import React, { FormEvent, useState } from "react";
@@ -6,9 +7,7 @@ import useSWR from "swr";
 import { Sub, Post } from "../../../../types";
 import Sidebar from "../../../components/Sidebar";
 
-interface submitProps {}
-
-const submit: React.FC<submitProps> = ({}) => {
+const submit: React.FC = () => {
   const router = useRouter();
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
@@ -92,3 +91,17 @@ const submit: React.FC<submitProps> = ({}) => {
 };
 
 export default submit;
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  try {
+    const cookie = req.headers.cookie;
+    if (!cookie) throw new Error("Missing auth token cookie");
+
+    await Axios.get("/auth/me", { headers: { cookie } });
+
+    return { props: {} };
+  } catch (err) {
+    res.writeHead(307, { Location: "/login" }).end();
+    return null;
+  }
+};
